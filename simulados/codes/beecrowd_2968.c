f9e82c57af2c70c193c7080c773cbea4261e9e2b
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2024-08-20 08:52:00 -0300

    Update beecrowd_2968.c

--- simulados/codes/beecrowd_2968.c
+++ simulados/codes/beecrowd_2968.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+/*
+Para compilar:
+gcc beecrowd_2968.c -lm -o prog
 
-using namespace std;
+Para executar:
+./prog
+*/
+
+#include<stdio.h>
+#include<math.h>
 
 int main(){
     int v,n;
@@ -8,7 +15,7 @@ int main(){
     
     scanf("%d %d",&v,&n);
     
-    total = ((long long int) v) * n);
+    total = ((long long int) v) * n;
     
     for(int i = 10; i < 90; i=i+10){
         printf("%lld ",(long long int) ceil(((double) total*i)/100.00));
