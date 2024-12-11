b7535aeb4f37c2582d549b6c8198cb4a0bdc9709
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2024-12-11 16:59:47 -0300

    Create beecrowd_1943.c

---
+++ simulados/codes/beecrowd_1943.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int k;
+    
+    scanf("%d",&k);
+    
+    if(k > 50){
+        printf("Top 100\n");
+    }
+    else if(k > 25){
+        printf("Top 50\n");
+    }
+    else if(k > 10){
+        printf("Top 25\n");
+    }
+    else if(k > 5){
+        printf("Top 10\n");
+    }
+    else if(k > 3){
+        printf("Top 5\n");
+    }
+    else if(k > 1){
+        printf("Top 3\n");
+    }
+    else{
+        printf("Top 1\n");
+    }
+ 
+    return 0;
+}
