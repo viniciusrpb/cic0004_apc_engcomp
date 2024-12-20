6cfa3dfb186c8ed23d5c0f527f91dae4bac2ac6d
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2024-12-20 16:55:20 -0300

    Update beecrowd_1366.c

--- repeticao/upsolving/beecrowd_1366.c
+++ repeticao/upsolving/beecrowd_1366.c
@@ -1,19 +1,46 @@
+/*
+Universidade de Brasília
+Departamento de Ciência da Computação
+CIC0004 - Algoritmos e Programação de Computadores
+Prof. Dr. Vinícius R. P. Borges
+
+Tópico: Estruturas de Repeticao
+Objetivo: Solução do problema beecrowd 1366 - Sticks Game (South America/Brazil Regional Contest - ACM/ICPC 2007)
+          https://www.beecrowd.com.br/judge/pt/problems/view/1366
+
+Comandos no Terminal do Linux para compilar e executar o codigo-fonte:
+
+gcc beecrowd_1366.c -o varetas
+./varetas
+*/
+
 #include <stdio.h>
+
+int solve(int n){
+ 
+   int i,c,v,paresVaretas;
+ 
+   paresVaretas = 0;
+
+   // ideia: sempre preciso de 2 pares de varetas para formar um triangulo
+ 
+   for(i = 0; i < n; i++){
+        scanf("%d %d",&c,&v);
+    
+        paresVaretas += v/2; // nao me interessa o resto dessa divisao!
+   }
+   
+   return paresVaretas/2;   
+}
  
 int main() {
  
-    int n,i,c,v,ans;
+    int n,ans;
     
     scanf("%d",&n);
     
     while(n!=0){
-        
-        ans = 0;
-        for(i = 0; i < n; i++){
-            scanf("%d %d",&c,&v);
-            ans += v/2;
-        }
-        ans = ans/2;
+        ans = solve(n);
         printf("%d\n",ans);
         scanf("%d",&n);
     }
