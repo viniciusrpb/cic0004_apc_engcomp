ee217b175a1200fe7e62e32e65f43baa8672eb8f
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-01-08 15:07:38 -0300

    Update beecrowd_1375.c

--- arrays/upsolving/beecrowd_1375.c
+++ arrays/upsolving/beecrowd_1375.c
@@ -1,39 +1,56 @@
 #include<stdio.h>
+#define MAX_N 1000 //tudo que eh MAX_N eh a mesma coisa do que 1000
+
+void solve(int n){
+    int i,pos_largada;
+    int car[MAX_N];
+    int pos[MAX_N];
+    int grid[MAX_N];
+    int existe_grid;
+
+    for(i = 0; i < n; i++){
+        scanf("%d %d",&car[i],&pos[i]);
+        grid[i] = 0; // garante que o vetor nao tem lixo
+    }
+
+    existe_grid = 1; // assume que existe o grid
+
+    // recriar o grid de largada
+    // pega o carro na i-esima posicao da corrida atual
+    for(i = 0; i < n; i++){
+        pos_largada = i + pos[i];
+
+        // garante que pos_largada eh uma posicao valida
+        // no vetor, no grid de largada
+        if(pos_largada >= 0 && pos_largada < n && grid[pos_largada] == 0){
+            grid[pos_largada] = car[i];
+        }
+        else{
+            existe_grid = 0; // nao eh possivel recuperar o grid de largada
+        }
+    }
+
+    if(existe_grid == 1){
+        for(i = 0; i < n-1; i++){
+            printf("%d ", grid[i]);
+        }
+        printf("%d\n",grid[n-1]);
+    }
+    else{
+        printf("-1\n");
+    }
+
+
+}
 
 int main(){
-    int i,n,c,p,resposta;
-    int grid[1000];
+    int n;
 
     scanf("%d",&n);
 
     while(n != 0){
 
-        // ainda eh possivel encontrar solucao valida
-        resposta = 1;
-
-        for(i = 0; i < n; i++){
-            grid[i] = 0; // nao tem carro na posicao i
-        }
-
-        for(i = 0; i < n; i++){
-            scanf("%d %d",&c,&p);
-            // a posicao de largada do carro c eh valida no grid?
-            // tem carro no grid na posicao i+p?
-            if((i+p) >= 0 && (i+p) < n && grid[i+p] == 0){
-                grid[i+p] = c;
-            }else{
-                resposta = -1;
-            }
-        }
-
-        if(resposta == 1){
-            for(i = 0; i < n-1; i++){
-                printf("%d ",grid[i]);
-            }
-            printf("%d\n",grid[n-1]);
-        }else{
-            printf("-1\n");
-        }
+        solve(n);
 
         scanf("%d",&n);
     }
