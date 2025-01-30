d92ffba0ec94b31d8b6b66da386e0097b85e07a5
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-01-29 22:05:00 -0300

    Update beecrowd_2312.c

--- registros/upsolving/beecrowd_2312.c
+++ registros/upsolving/beecrowd_2312.c
@@ -1,84 +1,88 @@
 #include<stdio.h>
-#define MAX_NOME 50
+#include<string.h>
 
 struct pais{
-    char nome[MAX_NOME+1]; // tem o char esp \0
-    int medalha[3];
+    char nome[1001]; // nome do pais + '\0'
+    int ouro, prata, bronze;
 };
 
-typedef struct pais Paihs;
+typedef struct pais Pais;
 
-int comparar_string(char str1[MAX_NOME+1], char str2[MAX_NOME+1]){
+int comparaString(char str1[1001], char str2[1001]){
 
-    int i;
+    int i,j,n,m;
 
-    for(i = 0; str1[i]!='\0' && str2[i]!='\0'; i++){
-        if(str1[i] != str2[i]){
-            if(str1[i] > str2[i]){
-                return 1;
-            }else{
-                return -1;
+    n = strlen(str1);
+    m = strlen(str2);
+
+    for(i = 0; i < m && i < n; i++){
+        if(str1[i] > str2[i]){
+            return i+1;
+        }
+        else{
+            if(str1[i] < str2[i]){
+                return -(i+1);
             }
         }
     }
-
-    if(str1[i] == '\0' && str2[i] == '\0'){
-        return 0; // strings iguais
-    }else{
-        if(str2[i] == '\0'){
-            return 1; // str1 > str2
-        }else{
-            return -1; // str1 < str2
+    // depois de sair do laco
+    if(str1[i] > str2[i]){
+        return (i+1);
+    }
+    else{
+        if(str1[i] < str2[i]){
+            return -(i+1);
+        }
+        else{
+            return 0;
         }
     }
 }
 
-void copiar_string(char destino[MAX_NOME+1], char fonte[MAX_NOME+1]){
-    int i;
-    for(i = 0; fonte[i] != '\0'; i++){
-        destino[i] = fonte[i];
-    }
-    destino[i] = '\0';
-}
+// ordena um vetor de structs
+void ordena(Pais v[500], int n){
 
-void troca(Paihs quadro[500], int i, int j){
-    int aux;
-    char outro[MAX_NOME+1];
+    int i,j;
+    Pais aux;
 
-    aux = quadro[i].medalha[0];
-    quadro[i].medalha[0] = quadro[j].medalha[0];
-    quadro[j].medalha[0] = aux;
-
-    aux = quadro[i].medalha[1];
-    quadro[i].medalha[1] = quadro[j].medalha[1];
-    quadro[j].medalha[1] = aux;
-
-    aux = quadro[i].medalha[2];
-    quadro[i].medalha[2] = quadro[j].medalha[2];
-    quadro[j].medalha[2] = aux;
-
-    //outro = quadro[i].nome;//errado!!!!
-    copiar_string(outro,quadro[i].nome);
-    copiar_string(quadro[i].nome,quadro[j].nome);
-    copiar_string(quadro[j].nome,outro);
-}
-
-void ordena(Paihs quadro[500], int n){
-    int i,j,k;
-
-    for(i = 0; i < n-1;i++){
+    for(i = 0; i < n-1; i++){
         for(j = i+1; j < n; j++){
-            k = 0; // k comeca no ind da medalha de ouro
-            while(quadro[i].medalha[k] == quadro[j].medalha[k]){
-                k++;
+            // se o i-esimo pais tem mais medalhas de ouro do que o j-esimo pais 
+            if(v[i].ouro < v[j].ouro){
+                // se sim, faz a troca
+                aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
             }
-            if(k <= 2){
-                if(quadro[i].medalha[k] < quadro[j].medalha[k]){
-                    troca(quadro,i,j);
-                }
-            }else{
-                if(comparar_string(quadro[i].nome,quadro[j].nome) > 0){
-                    troca(quadro,i,j);
+            else{
+                // se houver empate nas medalhas de ouro entre os paises i e j
+                if(v[i].ouro == v[j].ouro){
+                    // verifica se o i-esimo pais tem mais medalhas de prata do que o j-esimo pais, faz a troca 
+                    if(v[i].prata < v[j].prata){
+                        // se sim, faz a troca entre os paises, para que o i-esimo pais com mais
+                        // medalhas de ouro venha antes do j-esimo pais
+                        aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                    else{
+                        if(v[i].prata == v[j].prata){
+                            if(v[i].bronze < v[j].bronze){
+                                aux = v[i];
+                                v[i] = v[j];
+                                v[j] = aux;
+                            }
+                            else{
+                                if(v[i].bronze == v[j].bronze){
+                                    if(comparaString(v[i].nome,v[j].nome) > 0){
+                                        aux = v[i];
+                                        v[i] = v[j];
+                                        v[j] = aux;
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -86,22 +90,25 @@ void ordena(Paihs quadro[500], int n){
 }
 
 int main(){
-
     int i,n;
-    Paihs quadro[500];
+    Pais paises[500];
 
     scanf("%d",&n);
-
+    
     for(i = 0; i < n; i++){
         getchar();
-        scanf("%s %d %d %d",quadro[i].nome,&quadro[i].medalha[0],&quadro[i].medalha[1],&quadro[i].medalha[2]);
+        //Belgica 2 2 2
+        scanf("%s",paises[i].nome);
+        scanf("%d",&paises[i].ouro);
+        scanf("%d",&paises[i].prata);
+        scanf("%d",&paises[i].bronze);
     }
-
-    ordena(quadro,n);
+    
+    ordena(paises, n); // chamada da funcao
 
     for(i = 0; i < n; i++){
-        printf("%s %d %d %d\n",quadro[i].nome,quadro[i].medalha[0],quadro[i].medalha[1],quadro[i].medalha[2]);
+        printf("%s %d %d %d\n",paises[i].nome,paises[i].ouro,paises[i].prata,paises[i].bronze);
     }
-
+    
     return 0;
 }
