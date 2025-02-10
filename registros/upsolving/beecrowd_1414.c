36207f623fee886c000346a193249c790a47d31d
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-02-10 13:09:39 -0300

    Update beecrowd_1414.c

--- registros/upsolving/beecrowd_1414.c
+++ registros/upsolving/beecrowd_1414.c
@@ -16,9 +16,50 @@ gcc beecrowd_1414.c -o copa
 
 #include<stdio.h>
 
+#define MAX_PAISES 200
+#define MAX_NOME 11
+
+int solve(int t, int n){
+    int i,ans,total;
+    int rank[MAX_PAISES];
+    char nome[MAX_NOME];
+
+    // veja que o nome do time nao eh importante para a resolucao do problema
+    // assim vamos descartar os nomes dos times logo apos suas leituras
+    for(i = 0; i < t; i++){
+        getchar();
+        scanf("%s %d",nome,&rank[i]);
+    }
+
+    // calcula o total de pontos feitos pelos times
+    total = 0;
+    for(i = 0; i < t; i++){
+        total += rank[i];
+    }
+
+    // sabemos que a vitoria fornece 3 pontos e que um empate nos fornece 1 ponto, mas cada equipe no empate ganha
+    // 1 ponto, o que geram 2 pontos no somatorio total entre os times
+          
+    // assim resposta eh o total de pontos (3*n) validos subtraido do total de pontos feitos por todos os times
+    ans = 3*n - total;
+          
+    return ans;
+}
+
 int main(){
 
+    int n,t,ans;
     
+    scanf("%d %d",&t,&n);
+    
+    while(t != 0){
+        
+        ans = solve(t,n);
+        
+        printf("%d\n",ans);
+        
+        scanf("%d %d",&t,&n);
+    }
 
     return 0;
 }
