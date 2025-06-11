f5c71d9902d7b4830b2e0d5f5816bc2b40fe8fe2
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-06-11 19:09:32 -0300

    Update beecrowd_1234.c

--- strings/upsolving/beecrowd_1234.c
+++ strings/upsolving/beecrowd_1234.c
@@ -1,50 +1,96 @@
+/*
+Universidade de Brasília
+Departamento de Ciência da Computação
+CIC0004 - Algoritmos e Programação de Computadores
+Dr. Vinicius R. P. Borges
+
+Tópico: Strings
+Objetivo: Solução do problema beecrowd 1234 - Dancing Sentence
+          https://judge.beecrowd.com/pt/problems/view/1234
+
+Comandos no Terminal do Linux para compilar e executar o codigo-fonte:
+
+gcc beecrowd_1048.c -o dancante
+./dancante
+*/
+
 #include<stdio.h>
 #include<string.h>
-#define MAX_N 50
+
+int isUppercase(char c){
+    if(c >= 65 && c <= 90){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+char toUppercase(char c){
+    if(c >= 97 && c <= 122){
+        return c-32;
+    }
+    return c;
+}
+
+char toLowercase(char c){
+    if(c >=65 && c<=90){
+        return c+32;
+    }
+    return c;
+}
 
 int main(){
-    int t,n,i,j,last;
-    char str[MAX_N+1];
+    int i,n,imprimeMaiusculo;
+    char sentence[51];
 
-    while(scanf("%[^\n]s",str)!=EOF){
-        getchar(); // livre-se do '\n'
-        // pega o comprimento da string
-        n = strlen(str);
+    while(scanf("%[^\n]",sentence)!=EOF){
+        
+        getchar();
 
-        last = 0; //ultima letra minuscula -"faz de conta"
-
+        // variavel "flag": o valor 1 significa que o caractere sentence[i] a ser impresso eh maiusculo.
+        // Caso o valor seja zero, obrigatoriamente o caractere sentence[i] deve ser impresso minusculo
+        imprimeMaiusculo = 1;
+        
+        n = strlen(sentence);
+        
         for(i = 0; i < n; i++){
-            if(str[i] == ' '){
-                printf("%c",str[i]);
-            }else{
-                // se o ultimo caractere foi minusculo
-                if(last == 0){
-                    // se o atual caractere eh minusculo
-                    if(str[i] >= 97 && str[i] <= 122){
-                        // imprime ele jah maiusculo
-                        printf("%c",str[i]-32);
+
+            // se for espaco em branco, apenas imprime ele
+            if(sentence[i] == ' '){
+                printf("%c",sentence[i]);
+            } else {
+                // se o caractere sentence[i] for maiusculo
+                if(isUppercase(sentence[i]) == 1){
+
+                    // ... e o caractere sentence[i] tem que ser impresso obrigatoriamente maiusculo
+                    if(imprimeMaiusculo == 1){
+                        // apenas imprime o caractere sentence[i]
+                        printf("%c",sentence[i]);
+                    } else {
+                        // senao converte o caractere sentence[i] para minusculo
+                        printf("%c",toLowercase(sentence[i]));
                     }
-                    else{
-                        printf("%c",str[i]);
+                } else {
+                    // ... caso contrato, se o caractere sentence[i] for minusculo
+
+                    // ... e o caractere sentence[i] tem que ser impresso obrigatoriamente maiusculo
+                    if(imprimeMaiusculo == 1){
+                        // tem que converte-lo para maiusculo
+                        printf("%c",toUppercase(sentence[i]));
+                    } else {
+                        // senao apenas imprime o caractere sentence[i]
+                        printf("%c",sentence[i]);
                     }
-                    last=1; // o ultimo caractere impresso foi maisc
-                }else{
-                    // ultimo caractere foi maiusculo
-                    // proximo caractere tem q ser minusculo
-                    // mas se ele for maiusculo...
-                    if(str[i] >= 65 && str[i] <= 90){
-                        // imprime esse caractere minusculo
-                        printf("%c",str[i]+32);
-                    }else{
-                        printf("%c",str[i]);
-                    }
-                    last = 0;
+                }
+
+                if(imprimeMaiusculo == 1){
+                    imprimeMaiusculo = 0;
+                } else {
+                    imprimeMaiusculo = 1;
                 }
             }
         }
         printf("\n");
-
     }
-
     return 0;
 }
