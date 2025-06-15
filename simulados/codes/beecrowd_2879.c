a5de356b8fea717b12613dd9ff94b449193af5e5
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-06-15 17:06:20 -0300

    Update beecrowd_2879.c

--- simulados/codes/beecrowd_2879.c
+++ simulados/codes/beecrowd_2879.c
@@ -1,3 +1,19 @@
+/*
+Universidade de Brasília
+Departamento de Ciência da Computação
+CIC0004 - Algoritmos e Programação de Computadores
+Autor: Vinicius R. P. Borges
+
+Tópico: Estruturas de Repetição
+Objetivo: Solução do problema beecrowd 2879 - Desvendando Monty Hall
+          https://judge.beecrowd.com/pt/problems/view/2879
+
+Comandos no Terminal do Linux para compilar e executar o codigo-fonte:
+
+gcc beecrowd_2879.c -o hall
+./hall
+*/
+
 #include<stdio.h>
 
 int main(){
@@ -6,9 +22,17 @@ int main(){
     scanf("%d",&n);
     
     ans = 0;
-    
+
+    /* o jogador sempre escolhe a porta 1 */
+
+    /* simulacao de n jogadas */
     for(i = 0; i < n; i++){
+        /* leitura da porta que contem o carro */
         scanf("%d",&porta);
+
+        /* o apresentador abre uma porta qualquer que tenha o bode (!= de porta). O jogador muda para a outra porta que nao contem o bode */
+
+        /* o jogador somente leva o carro se a porta que contem o carro nao for a primeira */
         if(porta != 1){
             ans++;
         }
