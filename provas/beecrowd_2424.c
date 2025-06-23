78b6626001152e8da5cd45db2340c95ba10c43e8
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-06-23 08:42:52 -0300

    Create beecrowd_2424.c

---
+++ provas/beecrowd_2424.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int x,y;
+    
+    scanf("%d %d",&x,&y);
+    
+    if(x >= 0 && x <= 432 && y >= 0 && y <= 468){
+        printf('dentro\n');
+    } else {
+        printf('fora\n');
+    }
+
+    return 0;
+}
