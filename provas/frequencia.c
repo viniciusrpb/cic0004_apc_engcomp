e8bd8e3c46d8312179b2ea0c9af4075615d9f0f9
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-07-16 18:37:59 -0300

    Update frequencia.c

--- provas/frequencia.c
+++ provas/frequencia.c
@@ -1,13 +1,27 @@
+/*
+Universidade de Brasília
+Departamento de Ciência da Computação
+CIC0004 - Algoritmos e Programação de Computadores
+Autor: Vinicius R. P. Borges (adaptado de Beecrowd 2410)
+
+https://judge.beecrowd.com/en/problems/view/2410
+
+Comandos no Terminal do Linux para compilar e executar o codigo-fonte:
+
+gcc frequencia.c -o freq
+./freq
+*/
+
 #include <stdio.h>
  
 int main() {
  
-    int freq[200002];
+    int freq[200001];
     int ans,i,n,a;
     
     scanf("%d",&n);
     
-    for(i = 0; i <= 200002; i++){
+    for(i = 0; i < 200001; i++){
         freq[i] = 0;
     }
     
